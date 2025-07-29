e597f81b8ce61e902365421ba82206401b80842b
aikinssss <aikinsemma23@gmail.com>
2025-07-29 19:56:13 +0000

    Add files via upload

---
+++ GroovoApplication.java
@@ -0,0 +1,13 @@
+package com.groovo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroovoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroovoApplication.class, args);
+	}
+
+}
